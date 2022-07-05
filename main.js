30da5bdf783c2bb4cefae8a42158b6d7bdc6f049
Olivia <ojinnings@gmail.com>
2022-07-05 15:17:38 -0400

    Finish playGame function

--- main.js
+++ main.js
@@ -17,55 +17,33 @@ function computerPlay() {
     }
 };
 
-console.log(computerPlay())
+console.log(computerPlay());
 
 var computerSelection = computerPlay();
 var playerSelection = prompt("Rock, paper, or scissors?");
-
-/*
-function playGame(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "Tie"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats Rock"
-    }  else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beats paper"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors"
-    } else if (playerSelection === paper && computerSelection === scissors) {
-        return 
-    }
-    
-    
-          
-
-}*/
-
-
-
-
 
 function playGame(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie"
     } else if (playerSelection === "rock") {
-        if (computerSelection === scissors) {
+        if (computerSelection === "scissors") {
             return "You won! Rock beats scissors!"
         } else {
             return "You lost! Paper beats rock!"
         }
-    } else if (playerSelection === "")
-}
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You won! Paper beats rock!"
+        } else {
+            return "You lost! Scissors rockbeat paper!"
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You won! Scissors beat paper!"
+        } else {
+            return "You lost! Rock beats scissors!"
+        }
+    }
+};
 
-
-
-
-winning (computer)
-rock vs scissors x
-paper vs rock x
-scissors vs paper x
-
-losing (computer)
-scissors vs paper
-paper vs rock
-rock vs scissors
+playGame(playerSelection,computerSelection);
