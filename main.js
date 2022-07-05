bd88e5047f791b7e2192aaf15af0ac6150e41ffd
Olivia <ojinnings@gmail.com>
2022-07-05 15:08:41 -0400

    Create playgame function

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function computerPlay() {
 
     if (randomnum === 1) {
         return "Rock"
-    } else if (randomnum ===2) {
+    } else if (randomnum === 2) {
         return "Paper"
     } else {
         return "Scissors"
@@ -22,10 +22,50 @@ console.log(computerPlay())
 var computerSelection = computerPlay();
 var playerSelection = prompt("Rock, paper, or scissors?");
 
+/*
 function playGame(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie"
-    } else if (playerSelection === "Rock" && computerSelection === paper) {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose! Paper beats Rock"
+    }  else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors"
+    } else if (playerSelection === paper && computerSelection === scissors) {
+        return 
     }
+    
+    
+          
+
+}*/
+
+
+
+
+
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie"
+    } else if (playerSelection === "rock") {
+        if (computerSelection === scissors) {
+            return "You won! Rock beats scissors!"
+        } else {
+            return "You lost! Paper beats rock!"
+        }
+    } else if (playerSelection === "")
 }
+
+
+
+
+winning (computer)
+rock vs scissors x
+paper vs rock x
+scissors vs paper x
+
+losing (computer)
+scissors vs paper
+paper vs rock
+rock vs scissors
